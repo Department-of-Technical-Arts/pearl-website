03074e4ec4d97a9826f38940f586ac2520b9fe57
Manas Ashwin <37976250+manaschubby@users.noreply.github.com>
2024-03-20 22:30:44 +0530

    fix: shift env to next public

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,13 +5,13 @@ import { getStorage, ref } from "firebase/storage";
 
 function StartFirebase() {
 	const firebaseConfig = {
-		apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-		authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-		databaseURL: process.env.REACT_APP_DATABASE_URL,
-		projectId: process.env.REACT_APP_PROJECT_ID,
-		storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-		messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
-		appId: process.env.REACT_APP_APP_ID,
+		apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+		authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+		databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+		projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+		storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+		messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_ID,
+		appId: process.env.NEXT_PUBLIC_APP_ID,
 	};
 	const app = initializeApp(firebaseConfig);
 	const storageRef = ref;
